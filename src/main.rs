177152bc0c49abb18d51976dc466a6935dee1896
tvpeter <withtvpeter@gmail.com>
2022-09-23 11:59:09 +0100

    complete removing bill

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::io;
+use std::{io, collections::HashMap};
 
 fn user_input() -> Option<String> {
     let mut buffer = String::new();
@@ -41,7 +41,7 @@ mod menu {
     pub fn add_bill(bills: &mut Bills) {
         println!("Bill name: ");
         let name = match user_input() {
-            Some(input) => input,
+            Some(input) => input.to_lowercase(),
             None => return,
         };
 
@@ -61,11 +61,29 @@ mod menu {
             println!("Bill: {:?}, amount: {:?}", bill.name, bill.amount);
         }
     }
+
+    pub fn remove_bill(bills: &mut Bills) {
+        for bill in bills.get_all() {
+            println!("{:?}", bill);
+        }
+        println!("supply bill name: ");
+        let name = match user_input() {
+            Some(input) => input.to_lowercase(),
+            None => return,
+        };
+
+        if bills.remove(name.as_str()) {
+            println!("Bill removed successfully");
+        }else {
+            println!("Bill not found");
+        }
+    }
 }
 
 enum MainMenu {
     AddBill,
-    ViewBill
+    ViewBill,
+    RemoveBill,
 }
 
 impl MainMenu {
@@ -73,6 +91,7 @@ impl MainMenu {
         match input {
             "1" => Some(Self::AddBill),
             "2" => Some(Self::ViewBill),
+            "3" => Some(Self::RemoveBill),
             _ => None,
         }
     }
@@ -82,6 +101,7 @@ impl MainMenu {
         println!(" == Bill Manager ==");
         println!("1. Add Bill");
         println!("2. View Bills");
+        println!("3. Remove Bill");
         println!("");
         println!("Enter selection: ");
     }
@@ -94,20 +114,24 @@ pub struct Bill {
 }
 
 pub struct Bills {
-    inner: Vec<Bill>
+    inner: HashMap<String, Bill>
 }
 
 impl Bills {
     fn new() -> Self {
-        Self { inner: vec![] }
+        Self { inner: HashMap::new() }
     }
 
     fn add (&mut self, bill: Bill) {
-        self.inner.push(bill);
+        self.inner.insert(bill.name.to_string(), bill);
     }
 
     fn get_all(&self) -> Vec<&Bill> {
-        self.inner.iter().collect()
+        self.inner.values().collect()
+    }
+
+    fn remove(&mut self, name: &str) -> bool {
+        self.inner.remove(name).is_some()
     }
 }
 
@@ -121,6 +145,7 @@ fn main() {
         match MainMenu::from_str(input.as_str()) {
             Some(MainMenu::AddBill) => menu::add_bill(&mut bills),
             Some(MainMenu::ViewBill) => menu::view_bills(&bills),
+            Some(MainMenu::RemoveBill) => menu::remove_bill(&mut bills),
             None => return,
         }
     }
