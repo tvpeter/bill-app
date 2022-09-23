edd5d40981a54a9b8025566e25959c4093276b11
tvpeter <withtvpeter@gmail.com>
2022-09-23 02:20:59 +0100

    add bill struct

--- src/main.rs
+++ src/main.rs
@@ -39,6 +39,30 @@ impl MainMenu {
     }
 }
 
+#[derive(Debug, Clone)] 
+pub struct Bill {
+    name: String,
+    amount: f64
+}
+
+pub struct Bills {
+    inner: Vec<Bill>
+}
+
+impl Bills {
+    fn new() -> Self {
+        Self { inner: vec![] }
+    }
+
+    fn add (&mut self, bill: Bill) {
+        self.inner.push(bill);
+    }
+
+    fn get_all(&self) -> Vec<&Bill> {
+        self.inner.iter().collect()
+    }
+}
+
 fn main() {
     
     loop {
